57abba59052649f671d1b375e70e724f9905a39d
Joely <sjoely131@gmail.com>
2023-08-08 09:38:17 -0300

    mais rápido a validação

--- driver.py
+++ driver.py
@@ -10,13 +10,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+
 import requests
 
 class GetStatus():
     def __init__(self) -> None:
-        # Chrome options
         self.options = webdriver.ChromeOptions()
-        #self.options.add_argument("--headless") até resolver o qrcode no popup
         self.options.add_argument('--window-size=900,600')
         self.options.add_argument("--disable-extensions")
         self.options.add_argument("--disable-gpu")
@@ -28,39 +28,127 @@ class GetStatus():
 
         self.cookies_file = "whatsapp_cookies.pkl"
 
-        # check if cookies file exists
         if os.path.exists(self.cookies_file):
-            # load cookies from file
             with open(self.cookies_file, "rb") as f:
                 self.cookies = pickle.load(f)
-            # add cookies to options
             for cookie in self.cookies:
                 self.options.add_argument(f"--cookie={cookie['name']}={cookie['value']}")
 
-        # create driver instance
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.options)
         self.driver.get("https://web.whatsapp.com/")
 
     def save_cookies(self):
-        # get cookies from driver
         cookies = self.driver.get_cookies()
-        # save cookies to file
         with open(self.cookies_file, "wb") as f:
             pickle.dump(cookies, f)
 
     def seturl(self):
         self.url = f"https://web.whatsapp.com/send/?phone={self.id}&text&type=phone_number&app_absent=0"
-   
+
+    def test(self):
+        self.count = 3
+        self.time = self.count * 5
+        self.driver.implicitly_wait(5)  # Defina um tempo de espera implícita
+
+        try:
+            valid_element = self.driver.find_element(By.XPATH, "//div[@title='Type a message']")
+            self.a = valid_element.get_attribute("title")
+        except NoSuchElementException:
+            try:
+                invalid_element = self.driver.find_element(By.XPATH, "//div[contains(text(), 'Phone number shared via url is invalid.')]")
+                self.a = invalid_element.get_attribute("innerHTML")
+            except NoSuchElementException:
+                if self.count > 1:
+                    self.count -= 1
+                    self.test()
+                else:
+                    self.result = "Erro."
+            else:
+                if self.a == self.checkerTextInvalid:
+                    self.result = "Número Inválido"
+                else:
+                    self.result = "Erro."
+        else:
+            if self.a == self.checkerTextValid:
+                self.result = "Número Válido."
+            else:
+                self.result = "Sucesso."
+
+        print(self.result)
+        self.count = 3
+
+    def run(self, id):
+        self.options.add_argument('headless')
+        self.checkerTextInvalid = "Phone number shared via url is invalid."
+        self.checkerTextValid = "Type a message"
+        self.id = id
+
+        self.seturl()
+        self.driver.get(self.url)
+
+        self.test()
+
+        return self.result
+        
+    def login(self):
+        self.driver.get("https://web.whatsapp.com")
+
+    def github(self):
+        self.driver.get("https://github.com/soaresgabe/whatsapp-status")
+
+    @staticmethod
+    def check_login_status(driver):
+        try:
+            chat_list = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div._3j8Pd")))
+            return True
+        except:
+            return False
+        else:
+            self.driver.close()
+            self.chrome_options.add_argument('--headless')
+            driver.execute_script("window.open('https://web.whatsapp.com');")
+
+'''
+class GetStatus():
+    def __init__(self) -> None:
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('--window-size=900,600')
+        self.options.add_argument("--disable-extensions")
+        self.options.add_argument("--disable-gpu")
+        self.options.add_argument("--disable-dev-shm-usage")
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("start-maximized")
+        self.options.add_argument("disable-infobars")
+        self.options.add_argument("--disable-browser-side-navigation")
+
+        self.cookies_file = "whatsapp_cookies.pkl"
+
+        if os.path.exists(self.cookies_file):
+            with open(self.cookies_file, "rb") as f:
+                self.cookies = pickle.load(f)
+            for cookie in self.cookies:
+                self.options.add_argument(f"--cookie={cookie['name']}={cookie['value']}")
+
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.options)
+        self.driver.get("https://web.whatsapp.com/")
+
+    def save_cookies(self):
+        cookies = self.driver.get_cookies()
+        with open(self.cookies_file, "wb") as f:
+            pickle.dump(cookies, f)
+
+    def seturl(self):
+        self.url = f"https://web.whatsapp.com/send/?phone={self.id}&text&type=phone_number&app_absent=0"
+
+
     def test(self):
             self.count = 3
             self.time = self.count * 5
             try:
-                # Check if it is valid, if there is the "Type a message"
                 self.a = WebDriverWait(self.driver, self.time).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'))).get_attribute("title")
                 #/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]
             except TimeoutException: 
                 try:
-                    # Check if it is invalid, if there is the "Phone number shared via url is invalid."
                     self.a = WebDriverWait(self.driver, self.time).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div[1]'))).get_attribute("innerHTML")
                 except TimeoutException:                
                     if(self.count > 1):
@@ -83,12 +171,12 @@ class GetStatus():
             finally:
                 print(self.result)
                 self.count = 3
-                # self.driver.quit()
+
 
     def run(self, id):
         
-        self.options.add_argument('headless'); # not working?
-        self.waitText = "Starting chat" # not being used
+        self.options.add_argument('headless');
+        self.waitText = "Starting chat"
         self.checkerTextInvalid = "Phone number shared via url is invalid."
         self.checkerTextValid = "Type a message"
         self.id = id
@@ -102,14 +190,7 @@ class GetStatus():
         
     def login(self):
         self.driver.get("https://web.whatsapp.com")
-        #self.check_login_status()
-        # WebDriverWait(self.driver, 10)
-        # # Take a screenshot of the whole window
-        # screenshot = self.driver.get_screenshot_as_png()
-        # img = Image.open(BytesIO(screenshot))
-        # # Crop the image to the region containing the QR code
-        # qr_code_img = img.crop((100, 100, 350, 350))
-        # qr_code_img.save("Project\qr_code.png")
+
 
     def github(self):
         self.driver.get("https://github.com/soaresgabe/whatsapp-status")
@@ -117,7 +198,6 @@ class GetStatus():
 
     def check_login_status(driver):
         try:
-            # Check if the chat list element is present, which means the user is logged in
             chat_list = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div._3j8Pd")))
             return True
         except:
@@ -127,3 +207,6 @@ class GetStatus():
             self.chrome_options.add_argument('--headless')
             driver.execute_script("window.open('https://web.whatsapp.com');")
 
+   
+
+'''
