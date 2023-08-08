f60a13ef3bc09c935ea62d30f18a9cf5a3773eb6
Joely <sjoely131@gmail.com>
2023-08-08 11:08:25 -0300

    versão ok

---
+++ driver copy.py
@@ -0,0 +1,114 @@
+# import required modules
+import os
+import pickle
+from io import BytesIO
+from PIL import Image, ImageTk
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+
+import requests
+
+
+class GetStatus():
+    def __init__(self) -> None:
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('--window-size=900,600')
+        self.options.add_argument("--disable-extensions")
+        self.options.add_argument("--disable-gpu")
+        self.options.add_argument("--disable-dev-shm-usage")
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("start-maximized")
+        self.options.add_argument("disable-infobars")
+        self.options.add_argument("--disable-browser-side-navigation")
+
+        self.cookies_file = "whatsapp_cookies.pkl"
+
+        if os.path.exists(self.cookies_file):
+            with open(self.cookies_file, "rb") as f:
+                self.cookies = pickle.load(f)
+            for cookie in self.cookies:
+                self.options.add_argument(f"--cookie={cookie['name']}={cookie['value']}")
+
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.options)
+        self.driver.get("https://web.whatsapp.com/")
+
+    def save_cookies(self):
+        cookies = self.driver.get_cookies()
+        with open(self.cookies_file, "wb") as f:
+            pickle.dump(cookies, f)
+
+    def seturl(self):
+        self.url = f"https://web.whatsapp.com/send/?phone={self.id}&text&type=phone_number&app_absent=0"
+
+
+    def test(self):
+            self.count = 3
+            self.time = self.count *2
+            try:
+                self.a = WebDriverWait(self.driver, self.time).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'))).get_attribute("title")
+                #/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]
+            except TimeoutException: 
+                try:
+                    self.a = WebDriverWait(self.driver, self.time).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div[1]'))).get_attribute("innerHTML")
+                except TimeoutException:                
+                    if(self.count > 1):
+                        self.count -= 1
+                        self.test(self)
+                except:
+                    self.result = "Error." or "Erro"
+                else:
+                    if(self.a == self.checkerTextInvalid):
+                        self.result = "Numero Invalido"
+                    else:
+                        self.result = "Error." or "Erro"
+            except:
+                self.result = "Error." or "Erro"
+            else:
+                if(self.a == self.checkerTextValid):
+                    self.result = "Numero valido."
+                else:
+                    self.result = "Sucesso."
+            finally:
+                print(self.result)
+                self.count = 3
+
+
+    def run(self, id):
+        
+        self.options.add_argument('headless');
+        self.waitText = "Starting chat"
+        self.checkerTextInvalid = "Phone number shared via url is invalid."
+        self.checkerTextValid = "Type a message"
+        self.id = id
+
+        self.seturl()
+        self.driver.get(self.url)
+
+        self.test()
+
+        return self.result
+        
+    def login(self):
+        self.driver.get("https://web.whatsapp.com")
+
+
+    def github(self):
+        self.driver.get("https://github.com/soaresgabe/whatsapp-status")
+
+
+    def check_login_status(driver):
+        try:
+            chat_list = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div._3j8Pd")))
+            return True
+        except:
+            return False
+        else:
+            self.driver.close()
+            self.chrome_options.add_argument('--headless')
+            driver.execute_script("window.open('https://web.whatsapp.com');")
---
+++ ui copy.py
@@ -0,0 +1,161 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.filedialog as fd
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from driver import GetStatus
+import pandas as pd
+import numpy as np
+from PIL import Image, ImageTk
+import threading
+import time
+
+
+class UI():
+    
+    def __init__(self):        
+        self.root = tk.Tk()
+        self.driver = GetStatus()
+
+        self.root.geometry("550x550")
+        self.root.resizable(False, False)
+        self.root.title("WhatsApp Status")
+        self.root.iconbitmap("Project\logo.ico")
+
+        self.create_ui_elements()
+
+    def create_ui_elements(self):
+        self.create_header_frame()
+        self.create_message_list()
+        self.create_input_frame()
+
+    def create_header_frame(self):
+        self.header_frame = tk.Frame(self.root, bg="#25D366")
+        self.header_frame.pack(side=tk.TOP, fill=tk.X)
+
+        self.title_label = tk.Label(self.header_frame, text="WhatsApp Status", font=("Helvetica", 16), fg="white", bg="#25D366")
+        self.title_label.pack(side=tk.LEFT, expand=True)
+
+    def create_message_list(self):
+        self.message_frame = tk.Frame(self.root, bg="white")
+        self.message_frame.pack(fill=tk.BOTH, expand=True)
+
+        self.message_scrollbar = tk.Scrollbar(self.message_frame)
+        self.message_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.message_list = tk.Listbox(self.message_frame, yscrollcommand=self.message_scrollbar.set)
+        self.message_list.pack(fill=tk.BOTH, expand=True)
+
+        self.message_scrollbar.config(command=self.message_list.yview)
+
+    def create_input_frame(self):
+        self.input_frame = tk.Frame(self.root, bg="#F7F7F7", bd=5)
+        self.input_frame.pack(fill=tk.X, pady=10)
+
+        self.input_entry = tk.Entry(self.input_frame, font=("Helvetica", 20))
+        self.input_entry.pack(fill=tk.X, expand=True, padx=5, pady=3)
+        self.input_entry.bind("<Return>", self.add_message)
+
+        self.style = ttk.Style()
+        self.style.configure("Round.TButton", padding=3, relief="solid", borderwidth=0, font=("Helvetica", 12), shape="circle", background="#F7F7F7", corner_radius=10)
+
+        self.upload_button = ttk.Button(self.input_frame, text="Upload File", style="Round.TButton", command=self.upload_file)
+        self.downloadall_button = ttk.Button(self.input_frame, text="Download File", style="Round.TButton", command=self.download_all)
+
+        self.upload_button.pack(side=tk.LEFT, pady=1, padx=1)
+        self.downloadall_button.pack(side=tk.RIGHT, pady=1, padx=1)
+
+    def upload_file(self):
+        file_path = askopenfilename()
+        if file_path.endswith(('.csv', '.xlsx')):
+            self.data = pd.read_csv(file_path) if file_path.endswith('.csv') else pd.read_excel(file_path)
+
+            arr = np.array(self.data.columns[0])
+            data_list = self.data[arr].tolist()
+
+            self.resultCSV = {}
+            for index, n in enumerate(data_list, 1):
+                isDuplicate, duplicateIndex = False, 0
+
+                if n in self.resultCSV:
+                    isDuplicate = True
+                    duplicateIndex = list(self.resultCSV.keys()).index(n)
+
+                self.message_list.insert(tk.END, f"{index} - Validando {n}...")
+                self.message_list.see(tk.END)
+                self.root.update()
+
+                status = self.check(n) if not isDuplicate else "Número duplicado."
+                self.resultCSV[duplicateIndex] = status
+                self.resultCSV[n] = status
+                print(self.resultCSV[n])
+
+                self.root.update()
+
+            self.driver.login()
+
+            self.message_list.insert(tk.END, "", f"Sua verificação terminou para {len(data_list)} numeros.", "")
+            self.message_list.insert(tk.END, "Agora você pode baixar os resultados.")
+            self.message_list.see(tk.END)
+            self.root.update()
+
+    def download_all(self):
+            num_rows = self.data.shape[0]  # Obtém o número total de linhas no DataFrame
+    
+             # Adiciona os valores ao DataFrame de acordo com o índice das linhas
+            for index, value in enumerate(self.resultCSV.values()):
+                if index < num_rows:
+                  self.data.at[index, 'Status'] = value
+
+            file_path = asksaveasfilename(defaultextension='.csv', initialfile='Results.csv')
+            self.data.to_csv(file_path, index=False)
+
+    def checkOne(self):
+        self.input_entry.unbind("<Return>")
+        self.popup = tk.Toplevel(self.root)
+        self.setup_popup_window(self.popup, "Single Check")
+
+        title_label = tk.Label(self.popup, text="Username:", bg='#25D366', fg='white', font=("TkDefaultFont", 14))
+        title_label.pack(pady=(10, 0))
+
+        self.popup_entry = tk.Entry(self.popup, font=("TkDefaultFont", 14), bg='#fff', fg='black')
+        self.popup_entry.pack()
+
+        self.popup.bind("<Return>", lambda event, entry=self.popup_entry: self.popup_action(event))
+
+    def setup_popup_window(self, window, title):
+        window.title(title)
+        window.iconbitmap("Project\logo.ico")
+        window.geometry("200x100")
+        window.configure(bg='#25D366')
+        window.resizable(0, 0)
+
+        x = (self.root.winfo_screenwidth() - window.winfo_reqwidth()) / 2
+        y = (self.root.winfo_screenheight() - window.winfo_reqheight()) / 2
+        window.geometry("+%d+%d" % (x, y))
+
+        window.grab_set()
+
+    def popup_action(self, event):
+        popup_text = self.popup_entry.get()
+        self.message_list.insert(tk.END, popup_text)
+        self.popup.destroy()
+        self.root.bind("<Return>", lambda event, entry=self.input_entry: self.add_message(event))
+
+    def add_message(self, event):
+        text = self.input_entry.get()
+        self.check(text)
+
+    def check(self, id):
+        id = str(id)
+        
+        if len(id) == 11 or len(id) == 13 or len(id) == 12:
+            if len(id) == 13 or len(id) == 12:
+                number = id
+            else:
+                number = "55" + id
+            
+            status = self.driver.run(number)
+        else:
+            status = "Número inválido"
+        
+        return status
